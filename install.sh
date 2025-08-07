8a66ee533a4cce3b565ce314de50b71bf7474422
Nikita <main@evansvl.ru>
2025-08-07 12:47:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -72,3 +72,5 @@ if [ "$(docker-compose ps -q)" ]; then
 else
     print_error "The container failed to start. Check the build logs above or run 'docker-compose logs' for details."
 fi
+
+cd repo-update-bot
